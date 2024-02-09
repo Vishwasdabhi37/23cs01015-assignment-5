52e14efeac243260368a583121afa3e6e55a4cf7
Vishwasdabhi37 <152868443+Vishwasdabhi37@users.noreply.github.com>
2024-02-09 13:39:07 +0530

    Update 23CS01015_assignment5_qsn7.c

--- 23CS01015_assignment5_qsn7.c
+++ 23CS01015_assignment5_qsn7.c
@@ -1,53 +1,96 @@
+// #include <stdio.h>
+// int main()
+// {
+//     int m, n, p, q;
+//     printf("Enter the order of first matrix(m,n) : ");
+//     scanf("%d%d", &m, &n);
+//     printf("Enter the order of second matrix(p,q) : ");
+//     scanf("%d%d", &p, &q);
+//     if (n != p)
+//     {
+//         printf("Matrix multiplication is not possible\n");
+//         return 0;
+//     }
+//     int a[m][n], b[p][q], c[m][q];
+//     printf("Enter matrix A : ");
+//     for (int i = 0; i < m; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             scanf("%d", &a[i][j]);
+//         }
+//     }
+//     printf("Enter matrix B : ");
+//     for (int i = 0; i < p; i++)
+//     {
+//         for (int j = 0; j < q; j++)
+//         {
+//             scanf("%d", &b[i][j]);
+//         }
+//     }
+//     for (int i = 0; i < m; i++)
+//     {
+//         for (int j = 0; j < q; j++)
+//         {
+//             int x = 0;
+//             c[i][j] = 0;
+//             while (x < n)
+//             {
+//                 c[i][j] += a[i][x] * b[x][j];
+//                 x++;
+//             }
+//         }
+//     }
+//     for (int i = 0; i < m; i++)
+//     {
+//         for (int j = 0; j < q; j++)
+//         {
+//             printf("%d ", c[i][j]);
+//         }
+//         printf("\n");
+//     }
+
+//     return 0;
+// }
 #include <stdio.h>
-int main()
+#include <string.h>
+void bubblesort(int *arr, int size)
 {
-    int m, n, p, q;
-    printf("Enter the order of first matrix(m,n) : ");
-    scanf("%d%d", &m, &n);
-    printf("Enter the order of second matrix(p,q) : ");
-    scanf("%d%d", &p, &q);
-    if (n != p)
+    int issorted = 0;
+    while (size != 1)
     {
-        printf("Matrix multiplication is not possible\n");
-        return 0;
-    }
-    int a[m][n], b[p][q], c[m][q];
-    printf("Enter matrix A : ");
-    for (int i = 0; i < m; i++)
-    {
-        for (int j = 0; j < n; j++)
+        int val = 0;
+        for (int i = 0; i < size - 1; i++)
         {
-            scanf("%d", &a[i][j]);
-        }
-    }
-    printf("Enter matrix B : ");
-    for (int i = 0; i < p; i++)
-    {
-        for (int j = 0; j < q; j++)
-        {
-            scanf("%d", &b[i][j]);
-        }
-    }
-    for (int i = 0; i < m; i++)
-    {
-        for (int j = 0; j < q; j++)
-        {
-            int x = 0;
-            c[i][j] = 0;
-            while (x < n)
+            issorted = 1;
+            if (arr[i] > arr[i + 1])
             {
-                c[i][j] += a[i][x] * b[x][j];
-                x++;
+                int temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                issorted = 0;
             }
         }
-    }
-    for (int i = 0; i < m; i++)
-    {
-        for (int j = 0; j < q; j++)
+        if (issorted)
         {
-            printf("%d ", c[i][j]);
+            return;
         }
-        printf("\n");
+        size--;
+    }
+}
+int main()
+{
+    int arr[10];
+    printf("Enter the elements of the array : ");
+    for (int i = 0; i < 10; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    int size = sizeof(arr) / (sizeof(typeof(*arr)));
+    bubblesort(arr, size);
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", arr[i]);
     }
 
     return 0;
